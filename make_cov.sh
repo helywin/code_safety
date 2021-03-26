80f56118c1c8a78813aafef19b2083626db99aba
helywin <jiang770882022@hotmail.com>
2021-03-26 17:45:57 +0800

    make_cov.sh in cmake target

--- make_cov.sh
+++ make_cov.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+BINARY_DIR=$1
 COVERAGE_FILE=coverage.info
 REPORT_FOLDER=coverage_report
-lcov --rc lcov_branch_coverage=1 -c -d cmake-build-debug/CMakeFiles/test_gcov.dir -o ${COVERAGE_FILE}
-genhtml --rc genhtml_branch_coverage=1 ${COVERAGE_FILE} -o ${REPORT_FOLDER}
+echo "二进制目录""${BINARY_DIR}"
+lcov --rc lcov_branch_coverage=1 -c -d "${BINARY_DIR}" -o "${BINARY_DIR}/${COVERAGE_FILE}"
+genhtml --rc genhtml_branch_coverage=1 "${BINARY_DIR}/${COVERAGE_FILE}" -o ${REPORT_FOLDER}
